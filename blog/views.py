59c744a179555827d3067c784df81dd9836c6945
emanuelesanntoss <emanuelesanntoss@gmail.com>
2021-07-28 19:09:53 -0300

    Modified security site

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
@@ -7,14 +8,13 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 
-
-
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
     return render(request, 'blog/post_list.html', {'posts': posts})
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+@login_required
 def post_new(request):
      if request.method == "POST":
          form = PostForm(request.POST)
@@ -27,6 +27,7 @@ def post_new(request):
      else:
          form = PostForm()
      return render(request, 'blog/post_edit.html', {'form': form})
+@login_required
 def post_edit(request, pk):
      post = get_object_or_404(Post, pk=pk)
      if request.method == "POST":
@@ -39,9 +40,11 @@ def post_edit(request, pk):
      else:
          form = PostForm(instance=post)
      return render(request, 'blog/post_edit.html', {'form': form})
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
@@ -49,6 +52,7 @@ def post_publish(request, pk):
 def publish(self):
     self.published_date = timezone.now()
     self.save()
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
